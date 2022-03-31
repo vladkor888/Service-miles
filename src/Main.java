d30b841a00b2f22b9410ca29f7238eecdaca714f
vladkor888 <lidnn97_1997@mail.ru>
2022-03-31 12:03:32 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 2000;
+        int rub = 20;
+        int miles = amount / rub;
+        System.out.println("Ваши бонусные мили: " + miles);
+    }
+}
